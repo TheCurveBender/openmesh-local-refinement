9b77efd76fe3468baa3e16bb113b03d4a2e95184
Sven-Kristofer Pilz <sven.pilz+github@gmail.com>
2017-08-21 13:32:32 +0200

    Fix: Prevent holes on irregular faces.

--- main.cpp
+++ main.cpp
@@ -47,26 +47,45 @@ TrianglePoints triangle_points(const Mesh& mesh, const Mesh::FaceHandle& triangl
 
 void refine(Mesh& m, set<Mesh::FaceHandle> faces) {
     IrregularEdges irregular_edges;
+    bool repair_mode = false;
 
     while (!faces.empty() || !irregular_edges.empty()) {
         if (faces.empty() && !irregular_edges.empty()) {
+            repair_mode = true;
+
             for (const auto i : irregular_edges) {
                 const auto& edge = i.first;
-                faces.insert(m.face_handle(m.find_halfedge(edge.first, edge.second)));
+                const auto& f = m.face_handle(m.find_halfedge(edge.first, edge.second));
+
+                faces.insert(f);
             }
         }
 
         const auto f = *faces.begin();
+        const auto t = triangle_points(m, f);
         faces.erase(f);
 
-        // determine longest edge
-        const auto t = triangle_points(m, f);
-        const auto e = longest_edge(t);
+        // determine edge
+        short e;
+        if (repair_mode) {
+            /// TODO: Handle irregular faces in a better way.
+            // Now they are not split along the longest edge, but otherwise their will be holes.
+            if (irregular_edges.count({t[0].vertex, t[1].vertex}) == 1) {
+                e = 0;
+            } else if (irregular_edges.count({t[1].vertex, t[2].vertex}) == 1) {
+                e = 1;
+            } else if (irregular_edges.count({t[2].vertex, t[0].vertex}) == 1) {
+                e = 2;
+            } else {
+                assert(false);
+            }
+        } else {
+            e = longest_edge(t);
+        }
+
         const auto& left = t[e];
         const auto& right = t[(e + 1) % t.size()];
-
         const auto edge = make_pair(left.vertex, right.vertex);
-
 
         // split
         const auto repair = irregular_edges.count(edge) == 1;
@@ -74,9 +93,19 @@ void refine(Mesh& m, set<Mesh::FaceHandle> faces) {
                                      : m.add_vertex((left.point + right.point) / 2.0);
         const auto& opposite = t[(e + 2) % t.size()];
 
+        Mesh::Color face_color;
+        if (m.has_face_colors()) {
+           face_color = m.color(f);
+        }
+
         m.delete_face(f, false);
-        m.add_face(opposite.vertex, left.vertex, midpoint);
-        m.add_face(opposite.vertex, midpoint, right.vertex);
+        const auto first_half = m.add_face(opposite.vertex, left.vertex, midpoint);
+        const auto second_half = m.add_face(opposite.vertex, midpoint, right.vertex);
+
+        if (m.has_face_colors()) {
+            m.set_color(first_half, face_color);
+            m.set_color(second_half, face_color);
+        }
 
         if (repair) {
             irregular_edges.erase(edge);
@@ -88,13 +117,13 @@ void refine(Mesh& m, set<Mesh::FaceHandle> faces) {
             const auto opposite_half_edge = m.find_halfedge(edge.second, edge.first);
             if (opposite_half_edge.is_valid() && !m.is_boundary(opposite_half_edge)) {
                 irregular_edges.insert(make_pair(make_pair(edge.second, edge.first), midpoint));
-                faces.insert(m.face_handle(opposite_half_edge));
             }
         }
     }
 
     m.garbage_collection();
 }
+
 void test_vertex_split() {
     Mesh m;
 
